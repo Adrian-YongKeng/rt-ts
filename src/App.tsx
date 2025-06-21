a691847086082189d4576bba2fdce918e15cf360
Adrian <yy_1123@hotmail.com>
2025-06-21 21:34:48 +0800

    props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Greet from "./components/Greet";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Greet name="Adrian" />
+    </div>
+  );
+};
 
 export default App;
