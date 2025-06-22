4648f9521886a65ed8568f2d6371997af78f4e48
Adrian <yy_1123@hotmail.com>
2025-06-22 21:19:41 +0800

    event props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
+import Button from "./components/Button";
 import Greet from "./components/Greet";
 import Heading from "./components/Heading";
+import Input from "./components/Input";
 import Oscar from "./components/Oscar";
 import Person from "./components/Person";
 import PersonList from "./components/PersonList";
@@ -29,6 +31,14 @@ const App = () => {
 
   return (
     <div className="App">
+      <Input value="" handleChange={(event) => console.log(event)} />
+
+      <Button
+        handleClick={(event, id) => {
+          console.log("Button CLiked", event, id);
+        }}
+      />
+
       <Status status="error" />
 
       <Heading>Placeholder text</Heading>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,12 @@
-const Button = () => {
-  return <button>Click</button>;
+type ButtonProps = {
+  //   handleClick: () => void;
+  handleClick: (event: React.MouseEvent<HTMLButtonElement>, id: number) => void;
+};
+
+const Button = (props: ButtonProps) => {
+  return (
+    <button onClick={(event) => props.handleClick(event, 1)}>Click</button>
+  );
 };
 
 export default Button;
