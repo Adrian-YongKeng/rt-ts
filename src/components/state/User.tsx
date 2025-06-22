b40da0a56edee43979c66c3792b78cda5a5c7a1b
Adrian <yy_1123@hotmail.com>
2025-06-22 22:04:43 +0800

    type assertion

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -8,22 +8,28 @@ type AuthUser = {
 const User = () => {
   // specific angle bracket and mention the type of initial vale as well as the future value
   // user? user can be null
-  const [user, setUser] = useState<AuthUser | null>(null);
+  //  const [user, setUser] = useState<AuthUser | null>(null);
+
+  //type assertion if confirm data alway will be initialize after setup
+  const [user, setUser] = useState<AuthUser>({} as AuthUser);
 
   const handleLogin = () => {
     setUser({ name: "adrian", email: "adrian@123.com" });
   };
-  const handleLogout = () => {
-    setUser(null);
-  };
+  //   const handleLogout = () => {
+  //     setUser(null);
+  //   };
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
+      {/* <button onClick={handleLogout}>Logout</button> */}
 
       {/* user? user can be null  */}
-      <div>User name is {user?.name}</div>
-      <div>User email is {user?.email}</div>
+      {/* <div>User name is {user?.name}</div>
+      <div>User email is {user?.email}</div> */}
+
+      <div>User name is {user.name}</div>
+      <div>User email is {user.email}</div>
     </div>
   );
 };
