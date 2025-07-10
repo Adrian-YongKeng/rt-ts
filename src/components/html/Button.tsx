7d198b1e19bcd73d6352a28cc57bba20acd80466
Adrian <yy_1123@hotmail.com>
2025-07-10 15:29:15 +0800

    wrapping html elements (adding description)

--- src/components/html/Button.tsx
+++ src/components/html/Button.tsx
@@ -6,6 +6,11 @@ type ButtonProps = {
   // } & React.ComponentProps<"button">;
 } & Omit<React.ComponentProps<"button">, "children">;
 
+// TypeScript might allow children to be of type React.ReactNode (from React.ComponentProps<"button">), which is broader than string. This could lead to type mismatches or unexpected behavior, as the component expects children to be a string.
+// Omit is used to exclude the children prop from React.ComponentProps<'button'>
+// This allows the ButtonProps type to define its own children prop with a more specific type (string)
+// It ensure type safety and prevent conflicts between the explicity defined children and the one inherited from the HTML button props
+
 const CustomButton = ({ variant, children, ...rest }: ButtonProps) => {
   return (
     <button className={`class-with-${variant}`} {...rest}>
